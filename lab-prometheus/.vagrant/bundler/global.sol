23f65dfb6efcc4658e8ddc90d42e34d3893161de
unknown <higorribeiroaraujo007@gmail.com>
2023-06-27 11:11:43 -0300

    lab prometheus com grafana

---
+++ lab-prometheus/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.21.0"]],["vagrant-winnfsd",["= 1.4.0"]]],"checksum":"612ea9170129bf76c23a2e1a8ff3b7fb401798599b85e70bcdb0939bfa55005f","vagrant_version":"2.2.14"}
